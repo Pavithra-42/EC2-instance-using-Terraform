3503f590c9f00f42f40296d32121dd19418ad7cf
Pavithra-42 <157984965+Pavithra-42@users.noreply.github.com>
2024-03-03 17:20:26 +0530

    VPCmain.tf

--- main.tf
+++ main.tf
@@ -6,13 +6,13 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "tf-vpc" {
+resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "tf-vpc"
+    Name = "my-vpc"
   }
 }
 
