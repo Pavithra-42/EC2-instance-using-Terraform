58e3dd773617b5463504f0b48093b1eb2714da60
Pavithra-42 <157984965+Pavithra-42@users.noreply.github.com>
2024-03-03 16:13:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,62 @@
-resource "aws_instance" "instance_name" {
-    # Replace with ami-id with the ami id needs to be used
-    ami = "ami-08c40ec9ead489470"
-    instance_type = "t2.micro"
-    # VPC
-    subnet_id = "${subnet-071e013c3e6aebd3e}"
-    # Security Group
-    vpc_security_group_ids = [
-        "${sg-0a9fc10324fe8e760}"
-        ]
-    # the Public SSH key
-    key_name = "id_rsa"
-    connection {
-        type        = "ssh"
-        host        = self.public_ip
-        user        = "ubuntu"
-        private_key = file("${path.module}/id_rsa.pub")
-        timeout     = "4m"
-    }
-    tags = {
-        Name = "test_server"
-    }
+provider "aws" {
+  region = "us-east-1"  # Update with your desired AWS region
 }
-// Sends your public key to the instance
-resource "aws_key_pair" "id_rsa" {
-    key_name = "id_rsa"
-    public_key = file("${path.module}/id_rsa.pub")
 
-    tags = {
-        Name = "test_server"
-    }
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "example-vpc"
+  }
+}
+
+resource "aws_subnet" "example_subnet" {
+  vpc_id     = aws_vpc.example_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"  # Update with your desired availability zone
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "example-subnet"
+  }
+}
+
+resource "aws_security_group" "example_sg" {
+  name        = "example-sg"
+  description = "Allow SSH inbound traffic"
+
+  vpc_id = aws_vpc.example_vpc.id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "example_key_pair" {
+  key_name   = "example-key-pair"
+  public_key = file("~/.ssh/id_rsa.pub")  # Update with the path to your public key
+}
+
+resource "aws_instance" "example_instance" {
+  ami           = "ami-07d9b9ddc6cd8dd30"  # Update with your desired AMI ID
+  instance_type = "t2.micro"
+
+  key_name = aws_key_pair.example_key_pair.key_name
+
+  vpc_security_group_ids = [aws_security_group.example_sg.id]
+  subnet_id              = aws_subnet.example_subnet.id
+
+  tags = {
+    Name = "example-instance"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.example_instance.public_ip
 }
